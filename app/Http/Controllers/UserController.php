ad4c09941f3d7b7a9c883dd8369fc8128d048712
Difelice-e <difelice.emanuele@gmail.com>
2022-05-26 14:33:49 +0200

    redirect da fare dopo login/register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -66,9 +66,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        $users = User::findOrFail($slug);
+        return view('admin.users.show', compact('slug'));
     }
 
     /**
