fa97dd555c02be77ce9d0e7e185b20336dba78c6
matteopelusi7 <pelusimatteo02@gmail.com>
2022-05-30 17:43:40 +0200

    add view tipologies

--- resources/js/pages/Tipologies.index.vue
+++ resources/js/pages/Tipologies.index.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>
-      <ul>
-        <li v-for="tipology in tipologies" :key="tipology.id">
-          {{tipology.name}}
-        </li>
-      </ul>
+    <div class="container">
+        <div class="row pb-4">
+            <div class="col-12 text-center">
+                <h2 class="title-tipology font-weight-bold">Categorie</h2>
+            </div>
+        </div>
+        <ul class="row d-flex justify-content-center align-items-center tipology-list d-flex flex-wrap">
+            <li v-for="tipology in tipologies" :key="tipology.id" class="badge badge-pill badge-color tipologies col-4 col-md-3 col-lg-2">
+                {{tipology.name}}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -35,4 +40,34 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+.tipology-list {
+    list-style: none;
+    gap: 20px;
+    padding: 0;
+}
+
+.title-tipology {
+    font-size: 40px;
+}
+
+.badge-color {
+    background-color: #fff3da;
+}
+
+.badge-pill {
+    padding: 15px 60px;
+}
+
+.tipologies {
+    font-weight: 600;
+    font-size: 15px;
+}
+
+.tipologies:hover {
+    background-color: #ffc244;
+    cursor: pointer;
+}
+
+</style>
