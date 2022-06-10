123032a3769c7b9bdfc9a8bc0a7a9ab45a86be3d
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-06-10 23:42:31 +0200

    non completo

--- public/js/app.js
+++ public/js/app.js
@@ -37340,13 +37340,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-__webpack_require__(/*! C:\Users\Matteo\Desktop\Boolean\Progetto finale\deliverboo-team5\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Matteo\Desktop\Boolean\Progetto finale\deliverboo-team5\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! C:\Boolean\progetto-finale\deliverboo-team5\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Boolean\progetto-finale\deliverboo-team5\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> pagamenti_completati
+__webpack_require__(/*! C:\Users\giuse\Desktop\Boolean\PHP\Maggio 2022\prova\deliverboo-team5\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\giuse\Desktop\Boolean\PHP\Maggio 2022\prova\deliverboo-team5\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/js/pages/Checkout.vue
+++ resources/js/pages/Checkout.vue
@@ -1,29 +1,30 @@
 <template>
-    <div class="container">
-        <form @submit.prevent="handleSubmit(sendForm())" method="POST" class="py-5">
-            <div>
-                <label for="customer_name">Nome e Cognome</label>
-                <input v-model="form.customer_name" type="text" id="customer_name" name="customer_name">
+    <div class="container d-flex justify-content-center align-items-center flex-column py-5">
+        <h1 class="text-center">Aggiungi il tuo nome e la tua Email e un indirizzo di consegna</h1>
+        <form @submit.prevent="handleSubmit(sendForm())" method="POST" class="py-5 col-6 rounded ">
+            <div class="d-flex justify-content-center align-items-center flex-column flex-lg-row my-3">
+                <label class="mb-0 p-0 col-12 col-lg-2" for="customer_name">Nome e Cognome:</label>
+                <input class="col-12 col-lg-10 ml-0 ml-lg-2 p-1" placeholder="Mario Rossi" v-model="form.customer_name" type="text" id="customer_name" name="customer_name">
             </div>
 
-            <div>
-                <label for="customer_address">Indirizzo</label>
-                <input v-model="form.street_address" type="text" id="customer_address" name="customer_address">
+            <div class="d-flex justify-content-center align-items-center flex-column flex-lg-row my-3">
+                <label class="mb-0 p-0 col-12 col-lg-2" for="customer_address">Indirizzo:</label>
+                <input  class=" col-12 col-lg-10 ml-0 ml-lg-2 p-1" placeholder="Via Cavour, 12" v-model="form.street_address" type="text" id="customer_address" name="customer_address">
             </div>
 
-            <div>
-                <label for="customer_email">Email</label>
-                <input v-model="form.customer_email" type="text" id="customer_email" name="customer_email">
+            <div class="d-flex justify-content-center align-items-center flex-column flex-lg-row my-3">
+                <label class="mb-0 p-0 col-12 col-lg-2 " for="customer_email">Email:</label>
+                <input class=" col-12 col-lg-10 ml-0 ml-lg-2 p-1" placeholder="mariorossi88@gmail.com" v-model="form.customer_email" type="text" id="customer_email" name="customer_email">
             </div>
 
-            <div>
-                <label for="customer_telephone">Cellulare</label>
-                <input v-model="form.customer_phone" type="text" id="customer_telephone" name="customer_telephone">
+            <div class="d-flex justify-content-center align-items-center flex-column flex-lg-row my-3">
+                <label class="mb-0 p-0 col-12 col-lg-2" for="customer_telephone">Cellulare:</label>
+                <input class="col-12 col-lg-10 ml-0 ml-lg-2 p-1" placeholder="3285647385" v-model="form.customer_phone" type="text" id="customer_telephone" name="customer_telephone">
             </div>
         </form>
 
-        <button @click="sendForm()" type="submit">
-            <a href="/checkout">Vai al Pagamento</a>
+        <button class=" col-4 rounded-pill btn bg-greedgc" style="font-size: 20px;" @click="sendForm()" type="submit">
+            <a class="text-white weight-bold" href="/checkout">Vai al Pagamento</a>
         </button>
     </div>
 </template>
@@ -46,7 +47,7 @@ export default {
             axios.post('/api/orders', {
                 form: this.form,
                 total: localStorage.getItem('cartTotal'),
-                userId: this.localCart[0].user_id, 
+                userId: this.localCart[0].user_id,
             }, )
             .then( res => {
                 res = JSON.parse(res.config.data),
@@ -63,5 +64,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+input[type=text] {
+    background-color: transparent;
+    transition: border-bottom 0.4s ease-in-out;
+    border: none;
+    border-bottom: 2px solid gray;
+}
+
+input[type=text]:focus {
+    outline: none;
+    border-bottom: 2px solid #00a080;
+}
+
 </style>
